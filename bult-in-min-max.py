c6dd9cde3cbc65ce310371d58c95016df9b49c10
bkargel <105296779+bkargel@users.noreply.github.com>
2022-05-22 17:18:02 -0500

    Add files via upload

---
+++ bult-in-min-max.py
@@ -0,0 +1,25 @@
+"""Return the maximum of three values."""
+
+def maximum(value1,value2, value3):
+    
+    max_value = value1
+    if value2>max_value:
+        max_value = value2
+    if value3 > max_value:
+        max_value = value3
+    return max_value
+
+maximum(12, 27, 36)
+
+"""Return the minimum of three values."""
+def minimum(value1,value2, value3):
+
+    min_value = value1
+    if value2 < min_value:
+        min_value = value2
+    if value3 < min_value:
+        min_value = value3
+    return min_value
+
+minimum(12, 27, 36)
+
