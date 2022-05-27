346b3ac32ed6ddd79fd4484474855dcde7383eb7
bkargel <105296779+bkargel@users.noreply.github.com>
2022-05-27 14:24:00 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,14 @@
 # Using the math module to calculate square root of x
 # x = 900
 
-math.sqrt(900)
+import math
+
+square_root = math.sqrt(900)
 
 # Using the math module to round x to the largest integer not greater than x
 # x = 9.2
 
-math.floor(9.2)
+floor = math.floor(9.2)
+
+print('The square root of 900 is', square_root)
+print('The floor of 9.2 is', floor)
