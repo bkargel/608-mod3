ecb0110893be24790b7d40bde586b00068d12a00
bkargel <105296779+bkargel@users.noreply.github.com>
2022-05-27 13:52:43 -0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Using the math module to calculate square root of x
+# x = 900
+
+math.sqrt(900)
+
+# Using the math module to round x to the largest integer not greater than x
+# x = 9.2
+
+math.floor(9.2)
---
+++ population-dispersion.py
@@ -0,0 +1,7 @@
+#Calculate the population variance and population standard deviation for the following data set: 1, 3, 4, 2, 6, 5, 3, 4, 5, 2
+
+import statistics
+
+statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])   # population variance
+
+statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])    # poopulation standard deviation
