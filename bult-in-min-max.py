fd1544c4be7642b368e204802ec585b76809704c
bkargel <105296779+bkargel@users.noreply.github.com>
2022-06-03 10:57:25 -0500

    Update bult-in-min-max.py

--- bult-in-min-max.py
+++ bult-in-min-max.py
@@ -1,19 +1,20 @@
 """Return the maximum of three values."""
 
-def maximum(value1,value2, value3):
+def maximum(value1, value2, value3):
     
     max_value = value1
-    if value2>max_value:
+    if value2 > max_value:
         max_value = value2
     if value3 > max_value:
         max_value = value3
     return max_value
 
-maximum(12, 27, 36)
+print(maximum(12, 27, 36))
 
 """Return the minimum of four values."""
-def minimum(value1,value2, value3, value4):
 
+def minimum(value1, value2, value3, value4):
+    
     min_value = value1
     if value2 < min_value:
         min_value = value2
@@ -23,5 +24,4 @@ def minimum(value1,value2, value3, value4):
         min_value = value4
     return min_value
 
-minimum(15, 9, 27, 14)
-
+print(minimum(15, 9, 27, 14))
